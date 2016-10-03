2be2d279db7324b9af35f939aaa93c9ab5c80f8f
caseirouowotm8 <caseirouowotm8@users.noreply.github.com>
2016-10-03 17:42:48 +0100

    Added handshake in writenoncanonical
    Will send a SET code and wait for UA for VTIME/10 seconds, then retries connecting MAX_TRIES times. Exit code 1 if connection not established.
    TODO: State machine

--- TP2/Code/writenoncanonical.c
+++ TP2/Code/writenoncanonical.c
@@ -15,11 +15,30 @@
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
+#define MAX_TRIES 3
+#define FLAG 0x7e
+#define A 0x03
+#define C_SET 0x03
+#define C_UA 0x07
 
 volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
 {
+    
+    unsigned char SET[5];
+	SET[0] = FLAG;
+	SET[1] = A;
+	SET[2] = C_SET;
+	SET[3] = A^C_SET;
+	SET[4] = FLAG;
+	unsigned char UA[5];
+	UA[0] = FLAG;
+	UA[1] = A;
+	UA[2] = C_UA;
+	UA[3] = A^C_UA;
+	UA[4] = FLAG;
+    
     int fd,c, res;
     struct termios oldtio,newtio;
     char buf[255];
@@ -55,12 +74,12 @@ int main(int argc, char** argv)
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VTIME]    = 30;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
+    leitura do(s) prÃ³ximo(s) caracter(es)
   */
 
     tcflush(fd, TCIOFLUSH);
@@ -72,6 +91,20 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
+    for(i=0;i<MAX_TRIES&&strcmp(buf,UA)!=0;i++){
+	printf("Before write\n"); //DBG
+	res=write(fd,SET,5);
+	printf("After write\n"); //DBG
+	res=read(fd,buf,5);
+	buf[res]=0;
+	printf("After read\n"); //DBG
+	}
+
+	if(i>=3) {
+		printf("Exceeded number of connection attempts\n");
+		return 1;
+	}
+    
     size_t len;
     if(fgets(buf, sizeof(buf), stdin) != NULL){
       len = strlen(buf);
