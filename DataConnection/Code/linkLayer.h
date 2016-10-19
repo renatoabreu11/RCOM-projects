5c78dbe1c22b2e4781ac19534ce757bac01d014f
José Carlos <jose__carlos17@hotmail.com>
2016-10-19 21:41:00 +0100

    Updated linkLayer.h

--- DataConnection/Code/linkLayer.h
+++ DataConnection/Code/linkLayer.h
@@ -1,9 +1,5 @@
 #include "libraries.h"
 
-#define C_I0 0x00
-#define C_I1 0x40
-#define C_RR0 0x05
-#define C_RR1 0x9896e5		//check this one, supposed to be 10000101
 #define MAX_TRIES 3
 #define MAX_SIZE 10
 #define BAUDRATE B38400
@@ -12,10 +8,16 @@
 #define FALSE 0
 #define TRUE 1
 
+//Frame info
 #define FLAG 0x7e
 #define A 0x03
 #define C_SET 0x03
 #define C_UA 0x07
+#define C_I0 0x00
+#define C_I1 0x40
+#define C_RR0 0x05
+#define C_RR1 0x85
+
 #define ESCAPE 0x7d
 
 typedef struct LinkLayer {
@@ -38,7 +40,7 @@ typedef enum {startI, flagRCVI, aRCVI, cRCVI, BCC1I, DATAI, BCC2I, stopI} iState
 
 /**
 */
-//LinkLayer *initLink();
+LinkLayer *initLink();
 
 /**
 */
@@ -73,6 +75,11 @@ int connectTransmitter(int fd);
 /**
 */
 int writeDataFrame(int fd, char *frame, int size);
+
+/**
+ * Reads and stores the actual image data into frame
+*/
+int readImageData(char *frame, char byteRead, char *xorBCC, iState state);
 
 /**
  * State machine to read the I frame
